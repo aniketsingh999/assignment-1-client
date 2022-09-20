22fc7a3bef93f350951d1959e1a077dc588d0bbb
Aniket Kumar <anikets679@gmail.com>
2022-09-20 17:35:04 +0530

    Complete

--- src/components/Home/MeetingList/index.js
+++ src/components/Home/MeetingList/index.js
@@ -1,9 +1,17 @@
-const MeetingList = () => {
+const MeetingList = ({ meetings }) => {
   return (
     <ul className='meeting-list'>
-      <li>meeting list</li>
+      {meetings.forEach((meeting) => (
+        <li>
+          <Meeting key={Date.now()} meeting={meeting} />
+        </li>
+      ))}
     </ul>
   );
+};
+
+const Meeting = ({ meeting }) => {
+  return <div className='meeting'>{meeting.title}</div>;
 };
 
 export default MeetingList;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import CreateMeetingForm from './CreateMeetingForm';
 import MeetingList from './MeetingList';
 
 const Home = ({ authToken, user, isLoggedIn, setIsLoggedIn }) => {
+  const [meetings, setMeetings] = useState([]);
+  const [showMeetings, setShowMeetings] = useState(true);
+
+  const fetchMeetings = async () => {
+    try {
+      let newMeetings = (
+        await axios.post(`${process.env.REACT_APP_BACKEND_URL}/meeting/get`, {
+          authToken,
+        })
+      ).data.meetings.concat(
+        (
+          await axios.post(
+            `${process.env.REACT_APP_BACKEND_URL}/meeting/invites`,
+            {
+              authToken,
+            }
+          )
+        ).data.meetings
+      );
+
+      setMeetings(newMeetings);
+      console.log({ newMeetings });
+      console.log({ meetings });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeetings();
+  }, []);
+
   return (
     <div className='home'>
       <section className='create-meeting-form-container'>
@@ -9,7 +43,7 @@ const Home = ({ authToken, user, isLoggedIn, setIsLoggedIn }) => {
           {...{ authToken, user, isLoggedIn, setIsLoggedIn }}
         />
       </section>
-      <MeetingList />
+      <MeetingList {...{ meetings }} />
     </div>
   );
 };
